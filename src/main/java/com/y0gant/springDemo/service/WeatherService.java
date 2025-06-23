052b51594114ef9813d9b19b8cfc12b54ac90637
Y0gant <yogantfaye731@proton.me>
2025-06-23 05:34:38 +0530

    @Value annotation usage

--- src/main/java/com/y0gant/springDemo/service/WeatherService.java
+++ src/main/java/com/y0gant/springDemo/service/WeatherService.java
@@ -2,25 +2,26 @@ package com.y0gant.springDemo.service;
 
 import com.y0gant.springDemo.entity.Weather;
 import com.y0gant.springDemo.entity.WeatherResponse;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-@RequiredArgsConstructor
 public class WeatherService {
 
-    private static final String APIKEY = "bc40e54194e73b4b8d3bb9e8cff71547";
-
-    private static final String WEATHER = "https://api.openweathermap.org/data/2.5/weather?q={CITY}&units=metric&appid={APIKEY}";
-
+    private static final String WEATHER = "https://api.openweathermap.org/data/2.5/weather?q={CITY}&units=metric&appid={apiKEY}";
     private final RestTemplate restTemplate;
+    @Value("${weather.api.key}")
+    private String apiKEY;
 
+    public WeatherService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public Weather getWeather(String city) {
-        String finalURL = WEATHER.replace("{CITY}", city).replace("{APIKEY}", APIKEY);
+        String finalURL = WEATHER.replace("{CITY}", city).replace("{apiKEY}", apiKEY);
         ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalURL, HttpMethod.GET, null, WeatherResponse.class);
         WeatherResponse weatherResponse = response.getBody();
         if (weatherResponse != null) {
